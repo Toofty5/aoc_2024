eb94678553039ff8f62f191d03347b99c05d616f
Jay Cheung <jay.cheung@gmail.com>
2024-12-05 23:53:46 -0500

    fixing day 4

--- 4a.py
+++ 4a.py
@@ -6,10 +6,70 @@ total = 0
 
 for row in range(len(matrix)):
     for col in range(len(matrix[0])):
-        if matrix[row][col] == 'A' and 0 < row < len(matrix) - 1 and 0 < col < len(matrix[0]) - 1:
-            uldr = matrix[row-1][col-1] + "A" + matrix[row+1][col+1]
-            dlur = matrix[row+1][col-1] + "A" + matrix[row-1][col+1]
-            if uldr in ['MAS', 'SAM'] and dlur in ['MAS', 'SAM']:
+
+        # check above
+        if row >= 3:
+            #UL
+            if col >= 3:
+                check = ''
+                for x in range(4):
+                    check += matrix[row-x][col-x]
+                if check == "XMAS":
+                    print("UL")
+                    total += 1
+            #up
+            check = ''
+            for x in range(4):
+                check += matrix[row-x][col]
+            if check == "XMAS":
                 total += 1
+                print('UP')
+
+            #UR
+            if col <= len(matrix[0]) - 4:
+                check = ''
+                for x in range(4):
+                    check += matrix[row-x][col+x]
+                if check == "XMAS":
+                    total += 1
+                    print("UR")
+
+
+        # check below
+        if row < len(matrix) - 3:
+            #DL
+            if col >= 3:
+                check = ''
+                for x in range(4):
+                    check += matrix[row+x][col-x]
+                if check == "XMAS":
+                    total += 1
+                    print("DL")
+            #down
+            check = ''
+            for x in range(4):
+                check += matrix[row+x][col]
+            if check == "XMAS":
+                total += 1
+                print("DOWN")
+
+            #DR
+            if col <= len(matrix[0]) - 4:
+                check = ''
+                for x in range(4):
+                    check += matrix[row+x][col+x]
+                if check == "XMAS":
+                    total += 1
+                    print("DR")
+        
+        # check sides
+        if col >= 3 and matrix[row][col-3:col+1] == "SAMX":
+            total += 1
+            print("left")
+
+        if col < len(matrix[0]) and matrix[row][col:col+4] == "XMAS":
+            total += 1
+            print("right")
+
 
 print(total)
